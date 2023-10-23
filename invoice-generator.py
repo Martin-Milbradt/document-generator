49f518e3623b42de46a0a30e898734a81feb1663
Milli <onethenumbernottheword@gmail.com>
2023-10-23 10:39:02 +0200

    update in english

--- invoice-generator.py
+++ invoice-generator.py
@@ -6,17 +6,17 @@ from docxtpl import DocxTemplate
 
 # pip install -r requirements.txt
 base_dir = Path(__file__).parent
-word_template_path = base_dir / "Vorlage.docx"
-excel_path = base_dir / "Verträge.xlsx"
-word_dir = base_dir / "Rechnungen Word"
-pdf_dir = base_dir / "Rechnungen PDF"
+word_template_path = base_dir / "template.docx"
+excel_path = base_dir / "contracts.xlsx"
+word_dir = base_dir / "invoices word"
+pdf_dir = base_dir / "invoices pdf"
 
 # Create output folder for the word documents
 word_dir.mkdir(exist_ok=True)
 pdf_dir.mkdir(exist_ok=True)
 
 # Convert Excel sheet to pandas dataframe
-df = pd.read_excel(excel_path, sheet_name="Liste")
+df = pd.read_excel(excel_path, sheet_name="List")
 
 # Iterate over each row in df and render word document
 for record in df.to_dict(orient="records"):
